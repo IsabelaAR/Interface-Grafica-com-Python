ef5fa016118a8858f8ee6282413fee9026a0a590
Isabela Rosa <37556162+IsabelaAR@users.noreply.github.com>
2025-11-06 22:43:43 +0000

    cu

--- conectar.py
+++ conectar.py
@@ -3,10 +3,10 @@ import psycopg2
 # Criar conexão
 conexao = psycopg2.connect(
     database="postgresDB",
-    user="admin",
-    password="admin123",
-    host="127.8.8.1",
-    port="5433"
+    user="meu_usuario",
+    password="minha_senha",
+    host="127.0.0.1",
+    port="5432"
 )
 print("Conexão com o Banco de Dados aberta com sucesso!")
 
--- cria_tabela.py
+++ cria_tabela.py
@@ -1,11 +1,26 @@
 from conectar import conexao, meu_cursor
 
+conexao = psycopg2.connect(
+    database="meu_banco",
+    user="meu_usuario",
+    password="minha_senha",
+    host="177.0.0.1",
+    port="54320"
+)
+print("Conexão com o Banco de Dados aberta com sucesso!"
+      )
 # Criação da tabela
-meu_cursor.execute('''
-    CREATE TABLE PRODUTO IF NOT EXISTS PRODUTO(
-        CODIGO SERIAL PRIMARY KEY,
-        NOME VARCHAR(100) NOT NULL,
-        PRECO NUMERIC(10, 2) NOT NULL);''')
+meu_cursor = conexao.cursor()
+
+if __name__ == '__main__':
+    
+    meu_cursor.execute('''
+        CREATE TABLE PRODUTO IF NOT EXISTS PRODUTO(
+            CODIGO SERIAL PRIMARY KEY,
+            NOME VARCHAR(100) NOT NULL,
+            PRECO NUMERIC(10, 2) NOT NULL
+        );
+    ''')
 
 # just in case
 conexao.commit()
